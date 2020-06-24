ebd62b43a70b7e38f543152b6847a26458eca4e4
Julia Valerio <juliav286@gmail.com>
2020-06-24 13:35:34 -0300

    configurando detalhes

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,16 @@
+window.addEventListener("scroll", function(){
 
+  const bgNav = document.querySelector('nav');
+  
+  if (pageYOffset >= 50) {
+    bgNav.classList.remove("navbar")
+    bgNav.classList.add("navbar_script")
+  }
+  if (pageYOffset < 49){
+    bgNav.classList.add("navbar")
+    bgNav.classList.remove("navbar_script")
+  } 
+})
 //form validation with jQuery plugin: Validate
 let form = $(".form-contato");
 console.log(form);
@@ -38,65 +50,3 @@ window.addEventListener("scroll", function(){
     }
     button.hidden = true;
 })
-
-//scrol reveal 
-const sobre = document.querySelector("#sobre");
-const portfolioTop = document.querySelector(".rowTop");
-const portfolioM = document.querySelector(".rowMiddle");
-const portfolioB = document.querySelector(".rowBottom");
-
-window.addEventListener("scroll", function(){
-  switch(window.screen.availWidth){
-      case 800:
-        sobre.classList.add("sobre-show")
-        sobre.classList.remove("sobre-hidden")
-        portfolioTop.classList.add("portfolio-rowTop-show")
-        portfolioTop.classList.remove("portfolio-rowTop-hidden")
-        portfolioM.classList.add("portfolio-rowMiddle-show")
-        portfolioM.classList.remove("portfolio-rowMiddle-hidden")
-        portfolioB.classList.add("portfolio-rowBottom-show")
-        portfolioB.classList.remove("portfolio-rowBottom-hidden")
-        break;
-  }
-
-  if(pageYOffset >= 300){
-     sobre.classList.add("sobre-show")
-     sobre.classList.remove("sobre-hidden")
-  }
-  else{
-    sobre.classList.remove("sobre-show")
-     sobre.classList.add("sobre-hidden")
-  }
-  if(pageYOffset >= 400){
-    portfolioTop.classList.add("portfolio-rowTop-show")
-    portfolioTop.classList.remove("portfolio-rowTop-hidden")
-  }
-  else{
-    portfolioTop.classList.remove("portfolio-rowTop-show")
-    portfolioTop.classList.add("portfolio-rowTop-hidden")
-  }
-  if(pageYOffset >= 600){
-    portfolioM.classList.add("portfolio-rowMiddle-show")
-    portfolioM.classList.remove("portfolio-rowMiddle-hidden")
-  }
-  else{
-    portfolioM.classList.remove("portfolio-rowMiddle-show")
-    portfolioM.classList.add("portfolio-rowMiddle-hidden")
-  }
-  if(pageYOffset >= 700){
-    portfolioB.classList.add("portfolio-rowBottom-show")
-    portfolioB.classList.remove("portfolio-rowBottom-hidden")
-  }
-  else{
-    portfolioB.classList.remove("portfolio-rowBottom-show")
-    portfolioB.classList.add("portfolio-rowBottom-hidden")
-  }
-})
-
-
-
-
-
-
-
-
